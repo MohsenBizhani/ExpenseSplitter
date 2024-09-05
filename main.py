1da4edc72349cdb54010a8d6e7bbb832b9dfc386
Mohsen Bizhani <37871023+MohsenBizhani@users.noreply.github.com>
2024-09-05 12:59:22 +0330

    final commit

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+def calculate_split(total_amount: float, number_of_people: int, currency: str) -> None:
+    if number_of_people <= 1:
+        raise ValueError("Number of people must be greater than 1.")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    share_per_person: float = total_amount / number_of_people
+
+    print(f'total expenses: {currency}{total_amount}')
+    print(f'number of people: {number_of_people}')
+    print(f"Share per person: {currency}{share_per_person:.2f}")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main() -> None:
+    try:
+        total_amount: float = float(input("Enter total amount to be split: "))
+        number_of_people: int = int(input("Enter number of people to be split: "))
+        currency: str = str(input("Enter currency to split the amount: ")).upper()
 
-
-# Press the green button in the gutter to run the script.
+        calculate_split(total_amount, number_of_people, currency)
+    except ValueError as e:
+        print(e)
+    
+    
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
